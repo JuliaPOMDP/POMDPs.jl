b74ae3e7aadf03f5caf36efdbace937279e82a4a
Maxim Egorov <maxgegorov@gmail.com>
2015-07-18 18:34:01 -0700

    Changed Belief to sub-type of AbstractDistribution

--- src/belief.jl
+++ src/belief.jl
@@ -1,5 +1,5 @@
 
-abstract Belief
+abstract Belief <: AbstractDistribution
 
-update_belief(b::Belief, action::Any, obs::Any) = error("$(typeof(b)) does not implement update_belief")
-update_belief!(b::Belief, action::Any, obs::Any) = error("$(typeof(b)) does not implement update_belief!")
+update_belief(pomdp::POMDP, action::Any, obs::Any) = error("$(typeof(b)) does not implement update_belief")
+update_belief!(b::Belief, pomdp::POMDP, action::Any, obs::Any) = error("$(typeof(b)) does not implement update_belief!")
--- src/policy.jl
+++ src/policy.jl
@@ -1,6 +1,6 @@
 
 abstract Policy
 
-action(p::Policy, belief::Any) = error("$(typeof(p)) does not implement get_action")
-value(p::Policy, belief::Any) = error("$(typeof(p)) does not implement get_value")
+action(p::Policy, belief::Belief) = error("$(typeof(p)) does not implement get_action")
+value(p::Policy, belief::Belief) = error("$(typeof(p)) does not implement get_value")
 
