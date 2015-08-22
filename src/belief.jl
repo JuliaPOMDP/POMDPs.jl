a7c02016b37583c36d85e07117600d5b758b42c9
Maxim Egorov <maxgegorov@gmail.com>
2015-08-22 11:20:10 -0700

    Added a create_belief() function for belief initialization

--- src/belief.jl
+++ src/belief.jl
@@ -1,4 +1,5 @@
 
 abstract Belief <: AbstractDistribution
 
+create_belief(pomdp::POMDP) = error("$(typeof(b)) does not implement create_belief")
 update_belief!(b::Belief, pomdp::POMDP, bold::Belief, action::Any, obs::Any) = error("$(typeof(b)) does not implement update_belief!")
