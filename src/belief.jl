905fed90880861cf936cefca768cd952d0ae5293
Edward Balaban <ebalaban@stanford.edu>
2015-09-09 21:16:15 -0700

    Added initial_belief() and fixed the error messages for create_belief() and belief()

--- src/belief.jl
+++ src/belief.jl
@@ -1,5 +1,6 @@
 
 abstract Belief <: AbstractDistribution
 
-create_belief(pomdp::POMDP) = error("$(typeof(b)) does not implement create_belief")
-belief(pomdp::POMDP, bold::Belief, action::Any, obs::Any, belief::Belief=create_belief(pomdp)) = error("$(typeof(b)) does not implement belief")
+create_belief(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement create_belief")
+belief(pomdp::POMDP, bold::Belief, action::Any, obs::Any, belief::Belief=create_belief(pomdp)) = error("$(typeof(pomdp)) does not implement belief")
+initial_belief(pomdp::POMDP; belief = create_belief(pomdp)) = error("$(typeof(pomdp)) does not implement create_belief")
