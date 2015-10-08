89fbc53f53c15349441a487bdd315389c4ab3100
Zachary Sunberg <sunbergzach(at)gmail.com>
2015-10-07 23:52:15 -0700

    updated functions in belief

--- src/belief.jl
+++ src/belief.jl
@@ -7,12 +7,14 @@
 # For tools supportng belief updates see POMDPToolbox.jl
 
 abstract Belief <: AbstractDistribution
+abstract BeliefUpdater
 
 # returns an initial belief
-initial_belief(pomdp::POMDP, belief = create_belief(pomdp)) = error("$(typeof(pomdp)) does not implement initial_belief")
+# XXX (Zach) I got rid of the second argument for this
+initial_belief(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement initial_belief")
 
 # returns any belief 
-create_belief(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement create_belief")
+create_belief(updater::BeliefUpdater) = error("$(typeof(updater)) does not implement create_belief")
 
 # updates the belief given the old belief (belief_old), the action and the observation
-belief(pomdp::POMDP, belief_old::Belief, action::Any, obs::Any, belief_new::Belief=create_belief(pomdp)) = error("$(typeof(pomdp)) does not implement belief")
+update(updater::BeliefUpdater, belief_old::Belief, action::Action, obs::Observation, belief_new::Belief=create_belief(updater)) = error("$(typeof(updater)) does not implement update for belief_old::$(typeof(belief_old)), action::$(typeof(action)), obs::$(typeof(obs)), and belief_new::$(typeof(belief_new))")
