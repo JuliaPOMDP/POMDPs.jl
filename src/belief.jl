c4668942470c4459bd93b947af04b6d965bc7d1b
Zachary Sunberg <sunbergzach(at)gmail.com>
2015-10-10 16:25:23 -0700

    added pomdp_func macro (did not use it yet)

--- src/belief.jl
+++ src/belief.jl
@@ -10,7 +10,7 @@ abstract Belief <: AbstractDistribution
 abstract BeliefUpdater
 
 # returns an initial belief
-# XXX (Zach) I got rid of the second argument for this
+# XXX [Zach] I got rid of the second argument for this
 initial_belief(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement initial_belief")
 
 # returns any belief 
@@ -18,3 +18,5 @@ create_belief(updater::BeliefUpdater) = error("$(typeof(updater)) does not imple
 
 # updates the belief given the old belief (belief_old), the action and the observation
 update(updater::BeliefUpdater, belief_old::Belief, action::Action, obs::Observation, belief_new::Belief=create_belief(updater)) = error("$(typeof(updater)) does not implement update for belief_old::$(typeof(belief_old)), action::$(typeof(action)), obs::$(typeof(obs)), and belief_new::$(typeof(belief_new))")
+
+convert_belief(updater::BeliefUpdater, belief::Belief) = error("$(typeof(updater)) does not implement convert_belief for belief::$(typeof(belief))")
