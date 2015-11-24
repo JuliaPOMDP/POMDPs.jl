98633c1dac89a590ab235bcbffba4c5778b48c59
Maxim Egorov <maxgegorov@gmail.com>
2015-11-24 07:37:29 -0800

    removed depricated functions

--- src/distribution.jl
+++ src/distribution.jl
@@ -27,16 +27,14 @@ abstract AbstractSpace
 @pomdp_func lowerbound(s::AbstractSpace, i::Int)
 # returns bound of dim i
 @pomdp_func upperbound(s::AbstractSpace, i::Int)
-# returns distribution for dim i
-@pomdp_func Base.getindex(s::AbstractSpace, i::Int)
 
 @pomdp_func domain(s::AbstractSpace)
 
 # return a space type
 @pomdp_func states(pomdp::POMDP)
-@pomdp_func states(pomdp::POMDP, state::Any, sts::AbstractSpace=states(pomdp))
+@pomdp_func states(pomdp::POMDP, state::State, sts::AbstractSpace=states(pomdp))
 @pomdp_func actions(pomdp::POMDP)
-@pomdp_func actions(pomdp::POMDP, state::Any, acts::AbstractSpace=actions(pomdp))
+@pomdp_func actions(pomdp::POMDP, state::State, acts::AbstractSpace=actions(pomdp))
 @pomdp_func observations(pomdp::POMDP)
-@pomdp_func observations(pomdp::POMDP, state::Any, obs::AbstractSpace=observations(pomdp))
+@pomdp_func observations(pomdp::POMDP, state::State, obs::AbstractSpace=observations(pomdp))
 
