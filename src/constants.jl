e103f97f006f7b1a8085f1831bcab8ea824ee922
Maxim Egorov <maxgegorov@gmail.com>
2016-02-11 16:31:01 -0800

    added an assertion for solver_name input

--- src/constants.jl
+++ src/constants.jl
@@ -1,2 +1,10 @@
 # url to remote organization repo
 const REMOTE_URL = "https://github.com/sisl/"
+
+# supported solvers
+const SUPPORTED_SOLVERS = Set{AbstractString}(
+                          ["DiscreteValueIteration",
+                           "MCTS",
+                           "QMDP",
+                           "SARSOP",
+                           "POMCP"])
--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,6 @@
 # adds a registered solver
 function add(solver_name::AbstractString)
+    @assert solver_name in SUPPORTED_SOLVERS string("The solver: ", solver_name, " is not supported")
     full_url = string(REMOTE_URL, solver_name, ".jl")
     Pkg.clone(full_url)
     Pkg.build(solver_name)
