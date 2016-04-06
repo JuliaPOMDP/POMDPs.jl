f3236cfabc61cfdaecbf890d651536a90dec3224
Zachary Sunberg <sunbergzach@gmail.com>
2016-04-05 18:08:50 -0700

    updated solver to use @pomdp_func

--- src/solver.jl
+++ src/solver.jl
@@ -1,5 +1,4 @@
-
 abstract Solver{S,A,O}
 
-create_policy{S,A,O}(solver::Solver{S,A,O}, pomdp::POMDP{S,A,O}) = error("$(typeof(pomdp)) does not implement create_policy")
-solve{S,A,O}(solver::Solver{S,A,O}, pomdp::POMDP{S,A,O}, policy=create_policy(solver, pomdp)) = error("$(typeof(solver)) does not implement solve for model $(typeof(pomdp))")
+@pomdp_func create_policy{S,A,O}(solver::Solver{S,A,O}, pomdp::POMDP{S,A,O})
+@pomdp_func solve{S,A,O}(solver::Solver{S,A,O}, pomdp::POMDP{S,A,O}, policy=create_policy(solver, pomdp))
