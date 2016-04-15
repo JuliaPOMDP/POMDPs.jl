dc812474a0ae0ea3d89b24ea278341fee88e2c0b
Zachary Sunberg <sunbergzach@gmail.com>
2016-04-14 19:42:05 -0700

    got rid of default constructors

--- src/default_constructors.jl
+++
@@ -1,8 +0,0 @@
-# implements some default zero-argument constructors for bitstypes that do not have them (see issue #65)
-
-# don't need these anymore since create_state was brought back
-#=
-Base.Bool() = zero(Bool)
-Base.Int() = zero(Int)
-Base.Float64() = zero(Float64)
-=#
