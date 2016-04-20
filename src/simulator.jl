fed260b900d45648be596b96c4691220903e7e4c
Zachary Sunberg <sunbergzach@gmail.com>
2016-04-20 12:21:47 -0700

    made simulate arguments less strict to avoid ambiguity warnings

--- src/simulator.jl
+++ src/simulator.jl
@@ -10,9 +10,15 @@ Base type for an object defining how a simulation should be carried out
 abstract Simulator
 
 """
-    simulate{S,A,O,B}(simulator::Simulator, problem::POMDP{S,A,O}, policy::Policy, updater::Updater, initial_belief::B) 
-    simulate{S,A,B}(simulator::Simulator, problem::MDP{S,A}, policy::Policy, updater::Updater, initial_belief::B) 
+    simulate{S,A,O,B}(simulator::Simulator, problem::POMDP{S,A,O}, policy::Policy{B}, updater::Updater{B}, initial_belief::Union{B,AbstractDistribution{S}}) 
 
 Runs a simulation using the specified policy and returns the accumulated reward
 """
-@pomdp_func simulate(simulator::Simulator, problem::Union{POMDP,MDP}, policy::Policy, updater::Updater, initial_belief::AbstractDistribution)
+@pomdp_func simulate(simulator::Simulator, problem::POMDP, policy::Policy, updater::Updater, initial_belief::Any)
+
+"""
+    simulate{S,A}(simulator::Simulator, problem::MDP{S,A}, policy::Policy, initial_state::S) 
+
+Runs a simulation using the specified policy and returns the accumulated reward
+"""
+@pomdp_func simulate{S,A}(simulator::Simulator, problem::MDP{S,A}, policy::Policy, initial_state::S)
