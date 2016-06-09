8954648b984b3c9741b1cc7488876e90e0486dbf
autodocs <autodocs>
2016-06-09 23:58:05 +0000

    build based on e4c52fc

--- latest/gen_pdf.sh
+++
@@ -1,3 +0,0 @@
-cat index.md install.md get_started.md concepts.md def_pomdp.md def_solver.md api.md faq.md > docs.md
-pandoc -N --template=mytemplate.tex --variable mainfont="Palatino" --variable sansfont="Century Gothic" --variable monofont="Consolas" --variable fontsize=12pt --variable version=1.15.2 docs.md --latex-engine=xelatex --toc -o docs.tex
-pdflatex docs.tex
