0eb8dd20633267e7853e7d1df51b847f0445284e
MaximeBouton <maxim.bouton@gmail.com>
2017-07-11 11:33:14 -0700

    remove belief parameter to updater type

--- src/belief.jl
+++ src/belief.jl
@@ -7,13 +7,11 @@
 """
 Abstract type for an object that defines how the belief should be updated
 
-    B: belief type that parametrizes the updater
-
 A belief is a general construct that represents the knowledge an agent has
 about the state of the system. This can be a probability distribution, an
 action observation history or a more general representation.
 """
-abstract type Updater{B} end
+abstract type Updater end
 
 # TODO(max): should this be moved to pomdp.jl?
 """
