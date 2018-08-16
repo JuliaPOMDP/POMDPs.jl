d99604463b9110ed8ff0d3ccbf4ee38b74fa084d
rejuvyesh <mail@rejuvyesh.com>
2018-08-16 11:26:08 -0700

    modernize tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,45 +2,63 @@ using Test
 
 using POMDPs
 using Random
+
 mutable struct A <: POMDP{Int,Bool,Bool} end
-@test_throws MethodError n_states(A())
-@test_throws MethodError stateindex(A(), 1)
+@testset "implement" begin
 
-@test !@implemented discount(::A)
-POMDPs.discount(::A) = 0.95
-@test @implemented discount(::A)
+    @test_throws MethodError n_states(A())
+    @test_throws MethodError stateindex(A(), 1)
 
-@test !@implemented reward(::A,::Int,::Bool,::Int)
-@test !@implemented reward(::A,::Int,::Bool)
-POMDPs.reward(::A,::Int,::Bool) = -1.0
-@test @implemented reward(::A,::Int,::Bool,::Int)
-@test @implemented reward(::A,::Int,::Bool)
+    @test !@implemented discount(::A)
+    POMDPs.discount(::A) = 0.95
+    @test @implemented discount(::A)
 
-@test !@implemented observation(::A,::Int,::Bool,::Int)
-@test !@implemented observation(::A,::Bool,::Int)
-POMDPs.observation(::A,::Bool,::Int) = [true, false]
-@test @implemented observation(::A,::Int,::Bool,::Int)
-@test @implemented observation(::A,::Bool,::Int)
+    @test !@implemented reward(::A,::Int,::Bool,::Int)
+    @test !@implemented reward(::A,::Int,::Bool)
+    POMDPs.reward(::A,::Int,::Bool) = -1.0
+    @test @implemented reward(::A,::Int,::Bool,::Int)
+    @test @implemented reward(::A,::Int,::Bool)
+
+    @test !@implemented observation(::A,::Int,::Bool,::Int)
+    @test !@implemented observation(::A,::Bool,::Int)
+    POMDPs.observation(::A,::Bool,::Int) = [true, false]
+    @test @implemented observation(::A,::Int,::Bool,::Int)
+    @test @implemented observation(::A,::Bool,::Int)
+end
 
 mutable struct D end
 POMDPs.sampletype(::Type{D}) = Int
-@test @implemented sampletype(::D)
-@test sampletype(D()) == Int
-
 struct E end
-@test_throws MethodError sampletype(E)
-@test_throws MethodError sampletype(E())
+@testset "sampletype" begin
+    @test @implemented sampletype(::D)
+    @test sampletype(D()) == Int
 
-include("test_inferrence.jl")
-include("test_requirements.jl")
-include("test_generative.jl")
+    @test_throws MethodError sampletype(E)
+    @test_throws MethodError sampletype(E())
+end
+
+@testset "infer" begin
+    include("test_inferrence.jl")
+end
+
+@testset "require" begin
+    include("test_requirements.jl")
+end
+
+@testset "generative" begin
+    include("test_generative.jl")
+end
+
 # include("test_tutorials.jl")
-include("test_generative_backedges.jl")
 
-let
-    struct CI <: POMDP{Int,Int,Int} end
-    struct CV <: POMDP{Vector{Float64},Vector{Float64},Vector{Float64}} end
+@testset "genback" begin
+    include("test_generative_backedges.jl")
+end
 
+struct CI <: POMDP{Int,Int,Int} end
+struct CV <: POMDP{Vector{Float64},Vector{Float64},Vector{Float64}} end
+
+@testset "convert" begin
     @test convert_s(Vector{Float32}, 1, CI()) == Float32[1.0]
     @test convert_s(statetype(CI), Float32[1.0], CI()) == 1
     @test convert_s(statetype(CV), Float32[2.0,3.0], CV()) == [2.0, 3.0]
