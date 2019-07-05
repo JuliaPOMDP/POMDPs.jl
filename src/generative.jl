d9f93e228b5f1df32287ad69bd5140222b398faa
Zachary Sunberg <sunbergzach@gmail.com>
2019-07-05 13:04:17 -0700

    more minimal implementation

--- src/generative.jl
+++ src/generative.jl
@@ -1,19 +1,14 @@
 # generative model interface functions
 
-# deprecated
-# function generate_s end
-# function generate_o end
-# function generate_sr end
-# function generate_so end
-# function generate_or end
-# function generate_sor end
-
 @deprecate generate_s(args...) gen(Val(:s), args...)
 @deprecate generate_o(args...) gen(Val(:o), args...)
 @deprecate generate_sr(args...) gen(Val((:s,:r)), args...)
 @deprecate generate_so(args...) gen(Val((:s,:o)), args...)
 @deprecate generate_or(args...) gen(Val((:o,:r)), args...)
 @deprecate generate_sor(args...) gen(Val((:s,:o,:r)), args...)
+
+
+function gen end # always returns a NamedTuple
 
 """
     initialstate{S}(p::Union{POMDP{S},MDP{S}}, rng::AbstractRNG)
