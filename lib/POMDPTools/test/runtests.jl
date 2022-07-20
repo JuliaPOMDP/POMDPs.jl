d91c74e823c5ce2df7a557b1f685a2015ec2797b
Zachary Sunberg <zachary.sunberg@colorado.edu>
2022-07-20 16:44:49 -0700

    working on model tests

--- lib/POMDPTools/test/runtests.jl
+++ lib/POMDPTools/test/runtests.jl
@@ -5,10 +5,11 @@ using POMDPs
 using POMDPModels: BabyPOMDP, SimpleGridWorld
 
 import Random
-using Random: MersenneTwister
+using Random: MersenneTwister, AbstractRNG
 using DiscreteValueIteration: ValueIterationSolver
 
 @testset "POMDPTools.jl" begin
+    # Distributions
     include("distributions/test_bool.jl")
     include("distributions/test_deterministic.jl")
     include("distributions/test_implicit.jl")
@@ -16,7 +17,18 @@ using DiscreteValueIteration: ValueIterationSolver
     include("distributions/test_sparse_cat.jl")
     include("distributions/test_uniform.jl")
 
-    # include("model_tools/test_fully_observable_pomdp.jl")
+    # Model Tools
+    include("model_tools/test_fully_observable_pomdp.jl")
+    include("model_tools/test_generative_belief_mdp.jl")
+    include("model_tools/test_info.jl")
+    include("model_tools/test_matrices.jl")
+    include("model_tools/test_obs_weight.jl")
+    include("model_tools/test_ordered_spaces.jl")
+    include("model_tools/test_reward_model.jl")
+    include("model_tools/test_tabular.jl")
+    include("model_tools/test_terminal_state.jl")
+    include("model_tools/test_underlying_mdp.jl")
+    include("model_tools/test_visualization.jl")
 
     # include("belief_updaters/test_belief.jl")
     # include("belief_updaters/test_k_previous_observations.jl")
