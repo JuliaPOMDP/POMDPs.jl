b53dcc8c54f103a452d45d06cdb8f8676916ed1f
NeroBlackstone <gf7600gs@gmail.com>
2023-06-05 02:02:47 +0800

    revert runtest.jl

--- lib/POMDPTools/test/runtests.jl
+++ lib/POMDPTools/test/runtests.jl
@@ -16,33 +16,33 @@ import CommonRLInterface
 
 
 @testset "POMDPTools.jl" begin
-    # @testset "POMDPDistributions" begin
-    #     include("distributions/test_bool.jl")
-    #     include("distributions/test_deterministic.jl")
-    #     include("distributions/test_implicit.jl")
-    #     include("distributions/test_pretty_printing.jl")
-    #     include("distributions/test_sparse_cat.jl")
-    #     include("distributions/test_uniform.jl")
-    # end
+    @testset "POMDPDistributions" begin
+        include("distributions/test_bool.jl")
+        include("distributions/test_deterministic.jl")
+        include("distributions/test_implicit.jl")
+        include("distributions/test_pretty_printing.jl")
+        include("distributions/test_sparse_cat.jl")
+        include("distributions/test_uniform.jl")
+    end
 
-    # @testset "ModelTools" begin
-    #     include("model_tools/test_fully_observable_pomdp.jl")
-    #     include("model_tools/test_generative_belief_mdp.jl")
-    #     include("model_tools/test_info.jl")
-    #     include("model_tools/test_matrices.jl")
-    #     include("model_tools/test_obs_weight.jl")
-    #     include("model_tools/test_ordered_spaces.jl")
-    #     include("model_tools/test_reward_model.jl")
-    #     include("model_tools/test_tabular.jl")
-    #     include("model_tools/test_terminal_state.jl")
-    #     include("model_tools/test_underlying_mdp.jl")
-    #     include("model_tools/test_visualization.jl")
-    # end
+    @testset "ModelTools" begin
+        include("model_tools/test_fully_observable_pomdp.jl")
+        include("model_tools/test_generative_belief_mdp.jl")
+        include("model_tools/test_info.jl")
+        include("model_tools/test_matrices.jl")
+        include("model_tools/test_obs_weight.jl")
+        include("model_tools/test_ordered_spaces.jl")
+        include("model_tools/test_reward_model.jl")
+        include("model_tools/test_tabular.jl")
+        include("model_tools/test_terminal_state.jl")
+        include("model_tools/test_underlying_mdp.jl")
+        include("model_tools/test_visualization.jl")
+    end
 
-    # @testset "BeliefUpdaters" begin
-    #     include("belief_updaters/test_belief.jl")
-    #     include("belief_updaters/test_k_previous_observations_belief.jl")
-    # end
+    @testset "BeliefUpdaters" begin
+        include("belief_updaters/test_belief.jl")
+        include("belief_updaters/test_k_previous_observations_belief.jl")
+    end
 
     @testset "Policies" begin
         include("policies/test_alpha_policy.jl")
@@ -58,20 +58,20 @@ import CommonRLInterface
         include("policies/test_dict_policy.jl")
     end
 
-    # @testset "Simulators" begin
-    #     include("simulators/test_display.jl")
-    #     include("simulators/test_history_recorder.jl")
-    #     include("simulators/test_parallel.jl")
-    #     include("simulators/test_rollout.jl")
-    #     include("simulators/test_sim.jl")
-    #     include("simulators/test_stepthrough.jl")
-    # end
+    @testset "Simulators" begin
+        include("simulators/test_display.jl")
+        include("simulators/test_history_recorder.jl")
+        include("simulators/test_parallel.jl")
+        include("simulators/test_rollout.jl")
+        include("simulators/test_sim.jl")
+        include("simulators/test_stepthrough.jl")
+    end
 
-    # @testset "CommonRLIntegration" begin
-    #     include("common_rl_integration/test_common_rl.jl")
-    # end
+    @testset "CommonRLIntegration" begin
+        include("common_rl_integration/test_common_rl.jl")
+    end
 
-    # @testset "Testing" begin
-    #     include("testing/runtests.jl")
-    # end
+    @testset "Testing" begin
+        include("testing/runtests.jl")
+    end
 end
