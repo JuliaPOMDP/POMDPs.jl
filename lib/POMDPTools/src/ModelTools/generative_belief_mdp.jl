6113ea4c6131aed9943cf1ecb5d65360f2cd47fe
Zachary Sunberg <zachary.sunberg@colorado.edu>
2024-07-12 15:02:37 -0600

    added a few minor docs

--- lib/POMDPTools/src/ModelTools/generative_belief_mdp.jl
+++ lib/POMDPTools/src/ModelTools/generative_belief_mdp.jl
@@ -6,7 +6,7 @@ Create a generative model of the belief MDP corresponding to POMDP `pomdp` with 
 
 A belief is considered terminal when _all_ POMDP states in the support with nonzero probability are terminal.
 
-The default behavior when a terminal POMDP state is sampled from the belief is to transition to [`terminalstate`](@ref). This can be controlled by the `terminal_behavior` keyword argument. Using `terminal_behavior=ContinueTerminalBehavior(pomdp, updater)` will cause the MDP to keep attempting a belief update even when the sampled state is terminal. This can be further customized by providing `terminal_behavior` with a `Function` or callable object that takes arguments b, s, a, rng and returns a new belief (see the implementation of `ContinueTerminalBehavior` for an example).  
+The default behavior when a terminal POMDP state is sampled from the belief is to transition to [`terminalstate`](@ref). This can be controlled by the `terminal_behavior` keyword argument. Using `terminal_behavior=ContinueTerminalBehavior(pomdp, updater)` will cause the MDP to keep attempting a belief update even when the sampled state is terminal. This can be further customized by providing `terminal_behavior` with a `Function` or callable object that takes arguments `b, s, a, rng` and returns a new belief (see the implementation of `ContinueTerminalBehavior` for an example). You can customize behavior additionally using `determine_gbmdp_state_type`.
 """
 struct GenerativeBeliefMDP{P<:POMDP, U<:Updater, T, B, A} <: MDP{B, A}
     pomdp::P
